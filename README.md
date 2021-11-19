118dcb091dd58690a4cb926e570d8ece35d1235d
MintoD <tobythedev7@outlook.com>
2021-11-19 07:38:03 +0700

    Remove keep inventory feature

---
+++ README.md
@@ -0,0 +1,7 @@
+# KeepExperience
+
+Keep experience when players die
+
+# License
+
+Released under the [GPL-3.0](https://github.com/MintoD/KeepExperience/blob/main/LICENSE) license.
--- src/main/kotlin/com/mintod/keepexperience/Main.kt
+++ src/main/kotlin/com/mintod/keepexperience/Main.kt
@@ -8,19 +8,30 @@ import cn.nukkit.plugin.PluginBase
 class Main : PluginBase(), Listener {
 
     private var keepExperienceEnabled: Boolean = false
+<<<<<<< HEAD
+=======
     private var keepInventoryEnabled: Boolean = false
+>>>>>>> 2c728525a892c134f236c8198581297c5867f4ad
 
     override fun onEnable() {
         this.server.pluginManager.registerEvents(this, this)
 
+<<<<<<< HEAD
+        this.saveDefaultConfig()
+        this.keepExperienceEnabled = this.config.getBoolean("keepExperienceEnabled")
+=======
         this.saveDefaultConfig();
         this.keepExperienceEnabled = this.config.getBoolean("keepExperienceEnabled")
         this.keepInventoryEnabled = this.config.getBoolean("keepInventoryEnabled")
+>>>>>>> 2c728525a892c134f236c8198581297c5867f4ad
     }
 
     @EventHandler
     private fun onDeath(e: PlayerDeathEvent) {
         e.keepExperience = this.keepExperienceEnabled
+<<<<<<< HEAD
+=======
         e.keepInventory = this.keepInventoryEnabled
+>>>>>>> 2c728525a892c134f236c8198581297c5867f4ad
     }
 }
